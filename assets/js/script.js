4e6398466c50a7c1eedb08c4b8a4763716af5db4
egarcode95 <egarcode95@gmail.com>
2023-08-03 17:27:04 -0600

    feat: agregamos funciones con script

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,12 @@ console.log("Página en construción con patricio estrella");
 function clickPatricio(){
     alert("La mayonesa es un intrumento?")
 }
+
+function changeColor( elementHtml, color){
+
+   /*  alert("Que emoción, voy a cambiar mi color") */
+
+    console.log(elementHtml);
+    console.log(color);
+    elementHtml.style.color = color;
+}

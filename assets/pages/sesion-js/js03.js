58bb75d873fb40732310617440e8a9bd518c09ab
egarcode95 <egarcode95@gmail.com>
2023-08-09 14:32:15 -0600

    fefactor:operaciones booleanas

--- assets/pages/sesion-js/js03.js
+++ assets/pages/sesion-js/js03.js
@@ -51,55 +51,63 @@ console.log(age); // 26
 //console.log( colorFavorito ); // colorFavorito no está definido
 console.log(comidaFavorita); // pozole
 
-
-//-------------------condicional if-else---------------
+// ----------------- Condicional if-else --------------------
 /*
-Ejecuta una sentencia si una condicion especifica es evaluada como verdadera.
+ Ejecuta una sentencia si una condición específica es
+ evaluada como verdadera.
 
-sintaxis:
-    if ( condicion ) sentencia;
+ Sintaxis:
+    if ( condición ) sentencia;
 
-    if ( condicion ) {
+    if ( condición ) {
         sentencias;
     }
 
-    if ( condicion ) sentencia;
-    else sentencia_si_condicion_es_falsa;
-    
-    if ( condicion ){
-         sentencias;
-    }     
-    else{
-         sentencias;
+    if ( condición ) sentencia;
+    else sentencia_si_condición_es_falsa;
+
+    if ( condición ) {
+        sentencias;
+    }
+    else {
+        sentencias;
     }
 
-    if ( condicion1 ) sentencia;
-    else if (condicion2) sentencia;
-    else if (condicion3) sentenicia;
-    else if(condicin3) sentencia;
-    .....
-    else if (condicionN)sentenciaN;
-    else condicion;
+    if (condición) sentencia;
+    else if (condicion 2) sentencia;
+         else sentencia_si_condicion2_es_falsa;
+
+
+    if ( condicion1 ) sentencias;
+    else if ( condicion2 ) sentencia;
+    else if ( condicion3 ) sentencia;
+    else if ( condicion4 ) sentencia;
+     .....
+    else if ( condicionN ) sentencia;
+    else condición;   
+
 */
 
-const temperatura = 24;
-let mensaje = "Temperatura de: ";
-if(temperatura===22){
-    mensaje +=`${temperatura} grados centigrados`;
+const temperatura = 25;
+let mensaje = "Temperatura de ";
+
+if ( temperatura === 22 ){
+    mensaje += `${temperatura} grados centigrados es ideal` ; // mensaje = mensaje + nvoTexto
 }
-else if(temperatura >= 15 && temperatura <= 21){
-    mensaje += `${temperatura} grados centigrados es fria`;
+else if ( temperatura >= 15 && temperatura<= 21) {
+    mensaje += `${temperatura} grados centigrados es fría` ;
 }
-else if(temperatura>=23 && temperatura <= 30){
+else if( temperatura>=23 && temperatura <= 30 ){
+//else if( temperatura <= 30 &&  temperatura>=23 ){
     mensaje += `${temperatura} grados centigrados es calurosa`;
 }
 else {
-    mensaje = `La ${temperatura} grados centigrados no es idea =(`;
- }
+    mensaje = `${temperatura} grados centigrados no es ideal =(` ;    
+}
 
- console.log(mensaje);
+console.log(mensaje);
 
- //--------------- Condicional Switch --------------------------
+//--------------- Condicional Switch --------------------------
 /*
  La condicional Switch evalua una expresión y se compara
  con el valor de cada instancia en 'case' y se ejecuta las
@@ -122,27 +130,100 @@ else {
         sentencias;
         break;
   }
-  */
 
-  const setVelocidadVentilador = ( velocidad = 0 )=>{
+*/
+
+/**
+ * Establece la velocidad de un ventilador
+ * @param {number} velocidad del ventilador
+ * @return {number} mensaje de la velocidad establecida
+ */
+const setVelocidadVentilador = ( velocidad = 0 )=>{
     let mensaje;
     // la condicional switch utiliza la comparación estricta ( === )
-    switch (parseInt( velocidad) ) {
+    switch (  parseInt( velocidad) ) {
         case 0:
-            mensaje = "apagado";
-            break;
+            mensaje = "apagado"; break;
         case 1:
-            mensaje = "velocidad baja";
-            break;
+            mensaje = "velocidad baja"; break;
         case 2:
-            mensaje = "velocidad media";
-            break;
+            mensaje = "velocidad media"; break;
         default:
-            mensaje = "el nivel no existe";        
-            break;
+            mensaje = "el nivel no existe"; break;
     }
     return mensaje;
 }
-console.log(`La velocidad del ventilador está en ${setVelocidadVentilador(1)}`)
-console.log(`La velocidad del ventilador está en ${setVelocidadVentilador(8)}`)
-console.log(`La velocidad del ventilador está en ${setVelocidadVentilador("2")}`)
+console.log(`La velocidad del ventilador está en ${setVelocidadVentilador(1)}`); // baja
+console.log(`La velocidad del ventilador está en ${setVelocidadVentilador(8)}`); // no existe
+console.log(`valor: 2 ${setVelocidadVentilador(2)}`); // velocidad media
+console.log(`valor: "2" ${setVelocidadVentilador("2")}`); // velocidad media
+console.log(`valor: ? ${setVelocidadVentilador()}`); // apagado
+
+// console.log(`valor: ? ${setVelocidadVentilador( prompt("Velocidad", 1))}`); // velocidad baja
+
+//-------------Ejercicio estaciones del año------------------
+/*
+Preguntar por el numero de mes (prompt o DOM) del 1 al 12
+Desplegar de acuerdo al mes, un Alert o DOM, la estacion del año.
+
+mes, 12,1,2, invierno
+mes 3,4,5 = primavera
+mes 6 7 8 = verano
+mes 9,10,11_ otoño
+
+Realizar una version con if-else-elseif y otra con switch.
+*/
+
+/* let numMes = prompt("Ingresa el número de mes(if-else)");
+if (numMes >= 1 && numMes <= 12) {
+    if (numMes == 12 || numMes <= 2) {
+        alert("Estás en Invierno");
+    } else if (numMes <= 5) {
+        alert("Estás en Primavera");
+    } else if (numMes <= 8) {
+        alert("Estas en Verano");
+    } else alert("Estas en Otoño")
+
+}
+else {
+    alert("Ingresa un mes válido")
+}
+
+    let numeroMes = prompt("Ingresa el numero de mes(switch)");
+        
+        switch (  parseInt(numeroMes) ) {
+            case 12:
+            case 1:
+            case 2:
+                alert("Estás en Invierno"); break;
+            case 3:
+            case 4:
+            case 5:
+                alert("Estás en Primavera"); break;
+            case 6:
+            case 7:
+            case 8:
+                alert("Estas en Verano"); break;
+            case 9:
+            case 10:
+            case 11:
+                alert("Estas en Otoño"); break;
+            default :
+                alert("Ingresa un mes valido"); break;
+        }
+         */
+    //-----------------El operador ternario---------------------
+    /*
+    Es el unico operador de JavaScritp que tiene 3 operandos.
+    Generalmente se utiliza como opción a la sentencia if-else.
+
+    sintaxis:
+        condición ? expresionSiCondicionEsVerdadera :expresionSiCondicionEsFalsa;
+    */
+
+        const pagoTarjetaCredito = false;
+        let msj;
+        if (pagoTarjetaCredito === true) msj="Ha realizado el pago de su TC";
+        else msj = "No ha realizado el pago de TC"; 
+            
+        console.log(`El usuario ${msj}`);
